c0239b496e2e0785761aa8489a9b336509dcd92a
henrique24201 <hmoura050705@gmail.com>
2024-09-14 10:12:33 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,42 @@
+var textoDisplay="";
+var valor1 = 0;
+var operador ="";
+ 
+$(document).on("click",".botao",function(){
+    textoDisplay += $(this).val();
+    $(".display").val(textoDisplay);
+})
+$(document).on("click",".opera", function(){
+     operador = $(this).html();
+     valor1 = textoDisplay;
+     textoDisplay ="";
+     $(".display").val("");
+})
+  $(document).on("click","#ce", function(){
+      textoDisplay = "";
+      $(".display").val("");
+      valor1 = 0;
+  })
+$(document).on("click","#igual", function(){
+     var valor2 = textoDisplay;
+     textoDisplay = "";
+     $(".display").val("");
+
+ 
+     if (operador == "-"){
+         textoDisplay = parseFloat(valor1)-parseFloat(valor2)
+     }
+ 
+     if (operador == "+"){
+         textoDisplay = parseFloat(valor1)+parseFloat(valor2)
+     }
+ 
+     if (operador == "x"){
+         textoDisplay = parseFloat(valor1)*parseFloat(valor2)
+     }
+ 
+     if (operador == "/"){
+         textoDisplay = parseFloat(valor1)/parseFloat(valor2)
+     }
+        $(".display").val(textoDisplay);
+     })
---
+++ www/components/loader.js
@@ -0,0 +1,11 @@
+//This is javascript fine
+var textoDisplay="";
+$(document).on("click","#btn1",function(){
+      textoDisplay+="1";
+      $("display").val(textoDisplay);
+})
+var textoDisplay="";
+$(document).on("click","#btn2",function(){
+      textoDisplay+="2";
+      $("display").val(textoDisplay);
+})
